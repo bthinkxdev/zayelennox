9c012d84877d4e83c5d152e448c24e776f19f890
argue11 <anargh.mv@gmail.com>
2026-09-10 11:54:57 +0530

    shiprocket url changed

--- shipping/urls.py
+++ shipping/urls.py
@@ -10,6 +10,6 @@ from shipping.webhook_views import ShiprocketWebhookView
 app_name = "shipping"
 
 urlpatterns = [
-    path("webhooks/shiprocket/", ShiprocketWebhookView.as_view(), name="shiprocket-webhook"),
+    path("webhooks/courier-tracking/", ShiprocketWebhookView.as_view(), name="shiprocket-webhook"),
     path("check-serviceability/", check_serviceability_view, name="check-serviceability"),
 ]
